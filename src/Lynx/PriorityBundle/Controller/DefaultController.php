b26a366b8332b6206379a25a520a8de82135a234
Olena Zhuk (olenzhu826) <olenzhu826@student.polsl.pl>
2017-05-12 01:54:05 +0200

    Re-adding PriorityBundle.

--- src/Lynx/PriorityBundle/Controller/DefaultController.php
+++ src/Lynx/PriorityBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lynx\StatusBundle\Controller;
+namespace Lynx\PriorityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LynxStatusBundle:Default:index.html.twig');
+        return $this->render('LynxPriorityBundle:Default:index.html.twig');
     }
 }
--- src/Lynx/SprintBundle/Controller/DefaultController.php
+++ src/Lynx/SprintBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SprintBundle\Controller;
+namespace Lynx\SprintBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SprintBundle:Default:index.html.twig');
+        return $this->render('LynxSprintBundle:Default:index.html.twig');
     }
 }
