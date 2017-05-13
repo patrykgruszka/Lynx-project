c90733f05278d6c3f08e26a0b25d23068b70c0d9
Olena Zhuk (olenzhu826) <olenzhu826@student.polsl.pl>
2017-05-13 23:36:59 +0200

    Corrected routings.

--- src/Lynx/PriorityBundle/Controller/DefaultController.php
+++ src/Lynx/PriorityBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/")
+     */
     public function indexAction()
     {
         return $this->render('LynxPriorityBundle:Default:index.html.twig');
--- src/Lynx/SprintBundle/Controller/DefaultController.php
+++ src/Lynx/SprintBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/")
+     */
     public function indexAction()
     {
         return $this->render('LynxSprintBundle:Default:index.html.twig');
